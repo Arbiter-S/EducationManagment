755608b96b9493694d80f2d6d6368cab1f6d5857
mohamad sajadian <mohamadsajadian1380.ms@gmail.com>
2023-11-01 00:07:21 +0330

    feat: CRUD operation for Faculty

--- edu/urls.py
+++ edu/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("administration/", admin.site.urls),
-    path("",include("user.urls"))
+    path("",include("user.urls")),
+    path("",include("department.urls")),
 ]
