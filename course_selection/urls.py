229135af3e27ccaa4db228d8197256980cda5e06
mohamad sajadian <mohamadsajadian1380.ms@gmail.com>
2023-11-06 00:23:45 +0330

    feat:  GET /student/{pk/me}/term-courses/

--- course_selection/urls.py
+++ course_selection/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("student/<str:pk>/pass-course-report/", StudentPassCourseReport.as_view(), name='pass_course_report'),
+    path("student/<str:pk>/passed-courses/", StudentPassedCourseReport.as_view(), name='pass_course_report'),
+    path("student/<str:pk>/passing-courses/", StudentPassingCourseReport.as_view(), name="passing_course"),
 
 ]
--- course_selection/views.py
+++ course_selection/views.py
@@ -1,6 +1,5 @@
 from rest_framework.views import APIView
 from user.models import Student
-from user.serializer import StudentSerializer
 from course.serializers import *
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
@@ -8,10 +7,19 @@ from rest_framework.permissions import IsAuthenticated
 from permissions import IsITAdmin, IsITAdminOrIsEducationalAssistantOrIsSupervisor
 
 
-class StudentPassCourseReport(APIView):
+class StudentPassedCourseReport(APIView):
     permission_classes = [IsAuthenticated, IsITAdminOrIsEducationalAssistantOrIsSupervisor]
 
     def get(self, request, pk):
         student = get_object_or_404(Student, pk=pk)
-        serializer = ApprovedCoueseSerializer(instance=student.passed_courses, many=True)
+        serializer = ApprovedCourseSerializer(instance=student.passed_courses, many=True)
+        return Response(serializer.data)
+
+
+class StudentPassingCourseReport(APIView):
+    permission_classes = [IsAuthenticated, IsITAdminOrIsEducationalAssistantOrIsSupervisor]
+
+    def get(self, request, pk):
+        student = get_object_or_404(Student, pk=pk)
+        serializer = SemesterCourseSerializer(instance=student.passing_courses, many=True)
         return Response(serializer.data)
